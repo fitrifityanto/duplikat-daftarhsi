8e969863fe4fadc5cac7f9af269f13f975628b00
fitrifityanto <fitrifityanto@gmail.com>
2024-11-14 06:29:40 +0700

    add: section testimoni

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        '2': 'repeat(2, 200px)',
+        '3': 'repeat(3, 200px)',
+        '4': 'repeat(4, 200px)',
+      },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 50s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      } 
+    },
   },
   plugins: [],
 }
